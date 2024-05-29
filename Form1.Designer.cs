fc52f255be00abbc5f92d2f668a69054eae3e172
Andrew K <andr1ch05@mail.ru>
2024-05-30 01:19:54 +0300

    Release

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,10 +38,12 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button1.Location = new System.Drawing.Point(44, 124);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 118);
@@ -51,67 +53,83 @@
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button2.Location = new System.Drawing.Point(222, 124);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(125, 118);
             this.button2.TabIndex = 2;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button3.Location = new System.Drawing.Point(395, 124);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(125, 118);
             this.button3.TabIndex = 3;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(395, 272);
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(44, 272);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(125, 118);
             this.button4.TabIndex = 6;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button5.Location = new System.Drawing.Point(222, 272);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(125, 118);
             this.button5.TabIndex = 5;
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(44, 272);
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(395, 272);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(125, 118);
             this.button6.TabIndex = 4;
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(395, 420);
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(44, 420);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(125, 118);
             this.button7.TabIndex = 9;
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button8.Location = new System.Drawing.Point(222, 420);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(125, 118);
             this.button8.TabIndex = 8;
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(44, 420);
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.Location = new System.Drawing.Point(395, 420);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(125, 118);
             this.button9.TabIndex = 7;
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // label1
             // 
@@ -123,11 +141,22 @@
             this.label1.TabIndex = 10;
             this.label1.Text = "Игрок ";
             // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(395, 46);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(125, 29);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "Новая игра";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(570, 564);
+            this.Controls.Add(this.button10);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button8);
@@ -156,6 +185,7 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button10;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace lab5
@@ -13,25 +6,351 @@ namespace lab5
     public partial class Form1 : Form
     {
         private int player;
+        private int counter;
         public Form1()
         {
             InitializeComponent();
             player = 1;
+            counter = 0;
+            label1.Text = "Игрок 1";
         }
-
+        private void new_game()
+        {
+            counter = 0;
+            label1.Text = "Игрок 1";
+            player = 1;
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+        }
+        private bool check_win()
+        {
+            if (button1.Text == button2.Text && button2.Text == button3.Text && button2.Text != "")
+            {
+                MessageBox.Show("Поздравляем! Игрок " + player + " выиграл в этом раунде!");
+                new_game();
+                return true;
+            }
+            if (button4.Text == button5.Text && button5.Text == button6.Text && button5.Text != "")
+            {
+                MessageBox.Show("Поздравляем! Игрок " + player + " выиграл в этом раунде!");
+                new_game();
+                return true;
+            }
+            if (button7.Text == button8.Text && button8.Text == button9.Text && button8.Text != "")
+            {
+                MessageBox.Show("Поздравляем! Игрок " + player + " выиграл в этом раунде!");
+                new_game();
+                return true;
+            }
+            if (button1.Text == button5.Text && button5.Text == button9.Text && button5.Text != "")
+            {
+                MessageBox.Show("Поздравляем! Игрок " + player + " выиграл в этом раунде!");
+                new_game();
+                return true;
+            }
+            if (button3.Text == button5.Text && button5.Text == button7.Text && button5.Text != "")
+            {
+                MessageBox.Show("Поздравляем! Игрок " + player + " выиграл в этом раунде!");
+                new_game();
+                return true;
+            }
+            if (button1.Text == button4.Text && button4.Text == button7.Text && button4.Text != "")
+            {
+                MessageBox.Show("Поздравляем! Игрок " + player + " выиграл в этом раунде!");
+                new_game();
+                return true;
+            }
+            if (button2.Text == button5.Text && button5.Text == button8.Text && button5.Text != "")
+            {
+                MessageBox.Show("Поздравляем! Игрок " + player + " выиграл в этом раунде!");
+                new_game();
+                return true;
+            }
+            if (button3.Text == button6.Text && button6.Text == button9.Text && button6.Text != "")
+            {
+                MessageBox.Show("Поздравляем! Игрок " + player + " выиграл в этом раунде!");
+                new_game();
+                return true;
+            }
+            if (counter == 9)
+            {
+                MessageBox.Show("Ничья! В этом раунде никто не выиграл :(");
+                new_game();
+                return true;
+            }
+            return false;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            if (button1.Text == "X" ||  button1.Text == "O") 
+            {
+                return;
+            }
             switch (player)
             {
                 case 1:
+                    counter++;
                     button1.Text = "X";
-                    player = 2;
+                    if (!(check_win()))
+                    {
+                        player = 2;
+                        label1.Text = "Игрок 2";
+                    }
                     break;
                 case 2:
+                    counter++;
                     button1.Text = "O";
-                    player = 1;
+                    if (!(check_win()))
+                    {
+                        player = 1;
+                        label1.Text = "Игрок 1";
+                    }
                     break;
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "X" || button2.Text == "O")
+            {
+                return;
+            }
+            switch (player)
+            {
+                case 1:
+                    counter++;
+                    button2.Text = "X";
+                    if (!(check_win()))
+                    {
+                        player = 2;
+                        label1.Text = "Игрок 2";
+                    }
+                    break;
+                case 2:
+                    counter++;
+                    button2.Text = "O";
+                    if (!(check_win()))
+                    {
+                        player = 1;
+                        label1.Text = "Игрок 1";
+                    }
+                    break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "X" || button3.Text == "O")
+            {
+                return;
+            }
+            switch (player)
+            {
+                case 1:
+                    counter++;
+                    button3.Text = "X";
+                    if (!(check_win()))
+                    {
+                        player = 2;
+                        label1.Text = "Игрок 2";
+                    }
+                    break;
+                case 2:
+                    counter++;
+                    button3.Text = "O";
+                    if (!(check_win()))
+                    {
+                        player = 1;
+                        label1.Text = "Игрок 1";
+                    }
+                    break;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == "X" || button4.Text == "O")
+            {
+                return;
+            }
+            switch (player)
+            {
+                case 1:
+                    counter++;
+                    button4.Text = "X";
+                    if (!(check_win()))
+                    {
+                        player = 2;
+                        label1.Text = "Игрок 2";
+                    }
+                    break;
+                case 2:
+                    counter++;
+                    button4.Text = "O";
+                    if (!(check_win()))
+                    {
+                        player = 1;
+                        label1.Text = "Игрок 1";
+                    }
+                    break;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "X" || button5.Text == "O")
+            {
+                return;
+            }
+            switch (player)
+            {
+                case 1:
+                    counter++;
+                    button5.Text = "X";
+                    if (!(check_win()))
+                    {
+                        player = 2;
+                        label1.Text = "Игрок 2";
+                    }
+                    break;
+                case 2:
+                    counter++;
+                    button5.Text = "O";
+                    if (!(check_win()))
+                    {
+                        player = 1;
+                        label1.Text = "Игрок 1";
+                    }
+                    break;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Text == "X" || button6.Text == "O")
+            {
+                return;
+            }
+            switch (player)
+            {
+                case 1:
+                    counter++;
+                    button6.Text = "X";
+                    if (!(check_win()))
+                    {
+                        player = 2;
+                        label1.Text = "Игрок 2";
+                    }
+                    break;
+                case 2:
+                    counter++;
+                    button6.Text = "O";
+                    if (!(check_win()))
+                    {
+                        player = 1;
+                        label1.Text = "Игрок 1";
+                    }
+                    break;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button7.Text == "X" || button7.Text == "O")
+            {
+                return;
+            }
+            switch (player)
+            {
+                case 1:
+                    counter++;
+                    button7.Text = "X";
+                    if (!(check_win()))
+                    {
+                        player = 2;
+                        label1.Text = "Игрок 2";
+                    }
+                    break;
+                case 2:
+                    counter++;
+                    button7.Text = "O";
+                    if (!(check_win()))
+                    {
+                        player = 1;
+                        label1.Text = "Игрок 1";
+                    }
+                    break;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.Text == "X" || button8.Text == "O")
+            {
+                return;
+            }
+            switch (player)
+            {
+                case 1:
+                    counter++;
+                    button8.Text = "X";
+                    if (!(check_win()))
+                    {
+                        player = 2;
+                        label1.Text = "Игрок 2";
+                    }
+                    break;
+                case 2:
+                    counter++;
+                    button8.Text = "O";
+                    if (!(check_win()))
+                    {
+                        player = 1;
+                        label1.Text = "Игрок 1";
+                    }
+                    break;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button9.Text == "X" || button9.Text == "O")
+            {
+                return;
+            }
+            switch (player)
+            {
+                case 1:
+                    counter++;
+                    button9.Text = "X";
+                    if (!(check_win()))
+                    {
+                        player = 2;
+                        label1.Text = "Игрок 2";
+                    }
+                    break;
+                case 2:
+                    counter++;
+                    button9.Text = "O";
+                    if (!(check_win()))
+                    {
+                        player = 1;
+                        label1.Text = "Игрок 1";
+                    }
+                    break;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            new_game();
         }
     }
 }
